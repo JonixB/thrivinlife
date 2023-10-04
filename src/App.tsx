86507d6f22410871792baa683bb0888ad785ff54
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-04 13:10:51 -0600

    set a max width for the app

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-      <div>
+      <div className="mx-auto max-w-screen-xl px-4">
         <Navbar />
       </div>
     </div>
