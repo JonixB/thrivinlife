67cd2c825cdfd41bdd3ffa3f2b29fa14ec8e596f
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-05 16:45:54 -0600

    initializing how the task card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TaskCard from './components/TaskCard';
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      name: 'Morning walk',
+      completed: false,
+    },
+    {
+      id: '2',
+      name: 'Buy groceries',
+      completed: true,
+    },
+    // ... add more tasks
+  ]);
+
+  const toggleTask = (taskId: string) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    );
+  };
   return (
     <div className="App">
       <div className="mx-auto max-w-screen-xl">
         <Navbar />
+        <div className="bg-gray-100 min-h-screen p-4">
+          <TaskCard date="October 5, 2023" tasks={tasks} onTaskToggle={toggleTask} />
+          {/* You can add more TaskCard components for other days */}
+        </div>
       </div>
     </div>
   );
