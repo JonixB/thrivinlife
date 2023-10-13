75b967c69791c43e3c5249515377ee3c05c93d96
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-12 20:46:21 -0600

    modified to use react-router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React, { useState } from 'react';
-import TaskBody from './components/TaskBody';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import TaskBody from './components/TaskBody';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App h-screen flex flex-col max-w-screen-xl mx-auto">
+    <Router>
+      <div className="App h-screen flex flex-col max-w-screen-xl mx-auto">
         <Navbar />
         <div className="bg-gray-100 p-4 flex flex-col justify-center flex-grow">
-          <TaskBody /> 
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/tasks" element={<TaskBody />} />
+            <Route path="*" element={<Login />} /> {/* This redirects to login */}
+          </Routes>
         </div>
         <Footer />
-    </div>
+      </div>
+    </Router>
   );
 }
 
