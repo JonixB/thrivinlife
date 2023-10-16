93aefb67cb531a06ad6a341ca6a67da5ab1cd7fb
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-16 13:08:09 -0600

    setup initial login and signup functions

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -15,7 +15,34 @@ const Login: React.FC<LoginProps> = ({ onLogin, onSignUp }) => {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  
+  const handleSignUp = async (username: string, password: string) => {
+    const { data, error } = await supabase.auth.signUp({
+      email: username,
+      password: password,
+    });
+    
+    if (error) {
+      console.error(error.message);
+    }
+    if (data) {
+      console.log(data);
+    }
+  }
 
+  const handleSignIn = async (username: string, password: string) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: username,
+      password: password,
+    });
+    
+    if (error) {
+      console.error(error.message);
+    }
+    if (data) {
+      console.log(data);
+    }
+  }
   return (
     <div className="h-screen flex justify-center items-center bg-gray-100">
       <div className="bg-white p-8 rounded-lg shadow-md w-96">
@@ -25,11 +52,11 @@ const Login: React.FC<LoginProps> = ({ onLogin, onSignUp }) => {
 
         <div className="mb-4">
           <label className="block text-sm font-medium mb-2" htmlFor="username">Username</label>
-          <input 
-            id="username" 
-            type="text" 
-            placeholder="Enter username" 
-            value={username} 
+          <input
+            id="username"
+            type="text"
+            placeholder="Enter username"
+            value={username}
             onChange={(e) => setUsername(e.target.value)}
             className="w-full p-3 border rounded-md"
           />
@@ -37,18 +64,18 @@ const Login: React.FC<LoginProps> = ({ onLogin, onSignUp }) => {
 
         <div className="mb-6">
           <label className="block text-sm font-medium mb-2" htmlFor="password">Password</label>
-          <input 
-            id="password" 
-            type="password" 
-            placeholder="Enter password" 
-            value={password} 
+          <input
+            id="password"
+            type="password"
+            placeholder="Enter password"
+            value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="w-full p-3 border rounded-md"
           />
         </div>
 
         <div className="mb-4">
-          <button 
+          <button
             onClick={() => isSignUp ? onSignUp?.(username, password) : onLogin?.(username, password)}
             className="w-full bg-blue-500 text-white p-3 rounded-md hover:bg-blue-600 focus:outline-none"
           >
@@ -59,8 +86,8 @@ const Login: React.FC<LoginProps> = ({ onLogin, onSignUp }) => {
         <div className="text-center">
           <p className="text-sm">
             {isSignUp ? 'Already have an account?' : 'Don’t have an account?'}
-            <span 
-              onClick={() => setIsSignUp(!isSignUp)} 
+            <span
+              onClick={() => setIsSignUp(!isSignUp)}
               className="text-blue-500 cursor-pointer ml-2"
             >
               {isSignUp ? 'Sign In' : 'Sign Up'}
