4d55452b2e75bedbe5ea05e80710eb5dfc696b09
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-16 13:22:22 -0600

    env variables need to be prefixed by REACT_APP

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.THRIVIN_LIFE_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.THRIVIN_LIFE_ANON_KEY!;
+const SUPABASE_URL = process.env.REACT_APP_THRIVIN_LIFE_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_THRIVIN_LIFE_ANON_KEY!;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
