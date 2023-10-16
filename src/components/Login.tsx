35d330b8c8e93e87db908ac640121f975215658e
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-16 12:08:03 -0600

    initialize supabase connection

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,4 +1,10 @@
 import React, { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = process.env.THRIVIN_LIFE_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.THRIVIN_LIFE_ANON_KEY!;
+
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 interface LoginProps {
   onLogin?: (username: string, password: string) => void;
