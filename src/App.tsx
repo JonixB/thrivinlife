12de7ab64b7a62f6c780a16973a62605d36713d5
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-17 21:29:49 -0600

    check if a user is logged in or not to render navbar conditionally

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,22 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import TaskBody from './components/TaskBody';
 import Login from './components/Login';
+import { supabase } from './lib/helper/supabase';
 
 function App() {
+  const user = supabase.auth.user();
   return (
     <Router>
       <div className="App h-screen flex flex-col max-w-screen-xl mx-auto">
-        <Navbar />
+        {user && <Navbar />}
         <div className="bg-gray-100 p-4 flex flex-col justify-center flex-grow">
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/tasks" element={<TaskBody />} />
-            <Route path="*" element={<Login />} /> {/* This redirects to login */}
+            <Route path="*" element={<Login />} />
           </Routes>
         </div>
-        <Footer />
+        {user && <Footer />}
       </div>
     </Router>
   );
