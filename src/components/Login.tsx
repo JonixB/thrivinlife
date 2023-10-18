dfcee00ee159cc5f860d3dd8020e34a129dfbe9c
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-17 21:30:13 -0600

    remove supabase initialization and imported instead

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
 import { useNavigate } from 'react-router-dom';
+import { supabase } from '../lib/helper/supabase';
 
-const SUPABASE_URL = process.env.REACT_APP_THRIVIN_LIFE_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.REACT_APP_THRIVIN_LIFE_ANON_KEY!;
-
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 interface LoginProps {
   onLogin?: (username: string, password: string) => void;
