572572ef288e1b4a834ccc9b234c0548f1bf0056
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-18 13:14:49 -0600

    fix the error of type inference

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import TaskBody from './components/TaskBody';
 import Login from './components/Login';
 import { supabase } from './lib/helper/supabase';
+import { Session } from '@supabase/supabase-js';
 
 function App() {
-  const user = supabase.auth.user();
+  const [user, setUser] = useState<Session | null>(null);
+
+  useEffect(() => {
+    
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (data !== null) {
+        setUser(data as unknown as Session); 
+      }
+    };
+    fetchUser();
+    console.log(user);
+  }, []);
+
   return (
     <Router>
       <div className="App h-screen flex flex-col max-w-screen-xl mx-auto">
