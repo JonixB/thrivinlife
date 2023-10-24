72e5ecd446c3ea66d8e6b308a00cd57d54b22362
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-23 21:21:53 -0600

    remove reacttoastify import since it's already imported in index.tsx:

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { supabase } from '../lib/helper/supabase';
 import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 
 interface LoginProps {
