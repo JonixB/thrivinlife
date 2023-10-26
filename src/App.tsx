4d28457271e34dfda24b4add85821fa52a8c5c11
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-25 20:49:06 -0600

    create fetchAvatar URL function

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,23 @@ import { ToastContainer } from 'react-toastify';
 
 function App() {
   const [user, setUser] = useState<Session | null>(null);
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+
+  const fetchAvatarUrl = async (userId: string) => {
+    if (userId) {
+      const { data, error } = await supabase
+        .from('profile')
+        .select('profile_image')
+        .eq('user_id', userId)
+        .single();
+  
+      if (error) {
+        console.error('Error fetching avatar URL:', error);
+        return;
+      }
+      setAvatarUrl(data?.profile_image || null);
+    }
+  };
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -26,6 +43,7 @@ function App() {
         console.log('Auth state changed:', event);
         if (session) {
           setUser(session);
+          fetchAvatarUrl(session.user.id);
         } else {
           setUser(null);
         }
@@ -40,11 +58,11 @@ function App() {
   return (
     <Router>
       <div className="App h-screen flex flex-col max-w-screen-xl mx-auto">
-        {user && <Navbar />}
+        {user && <Navbar avatarUrl={avatarUrl} />}
         <div className="bg-gray-100 p-4 flex flex-col justify-center flex-grow">
           <Routes>
             <Route path="/login" element={user ? <Navigate to="/tasks" /> : <Login />} />
-            <Route path="/tasks" element={user ? <TaskBody /> : <Navigate to="/login" />} />
+            <Route path="/tasks" element={user ? <TaskBody avatarUrl={avatarUrl} /> : <Navigate to="/login" />} />
             <Route path="*" element={<Navigate to={user ? "/tasks" : "/login"} />} />
           </Routes>
         </div>
