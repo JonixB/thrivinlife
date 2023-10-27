df5878d462daba5eb9bb312301b000285a967f22
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-26 21:19:48 -0600

    corrected column name misspelling

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -1,5 +1,5 @@
 -- Drop tables if they exist
-DROP TABLE IF EXISTS Profile, Task CASCADE;
+DROP TABLE IF EXISTS profiles, tasks CASCADE;
 
 -- Create tables
 CREATE TABLE profiles (
@@ -23,6 +23,6 @@ INSERT INTO profiles (user_id, profile_image)
 VALUES
     ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'https://avatars.githubusercontent.com/u/113629390?s=400&u=4a9ec4e8bca76b6c78f36952c3b973f34b3ff8c6&v=4');
 
-INSERT INTO tasks (user_id, task_title, taks_description, due_date, priority, status)
+INSERT INTO tasks (user_id, task_title, task_description, due_date, priority, status)
 VALUES
     ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'Coding', 'Keep coding to enhance my skills', '2023-10-24', 'High', 'Complete');
