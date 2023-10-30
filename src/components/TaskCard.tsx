9525cf60135db39e3d706f1fb9b2e3fe6c431e2e
Nikko Bansil <bjohnnikko@gmail.com>
2023-10-30 13:25:40 -0600

    re-style task card

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import avatar from '../assets/images/avatar.jpg';
+import { FaCheckCircle, FaTimesCircle } from 'react-icons/fa';
 
 interface Task {
   id: string,
@@ -26,51 +27,52 @@ const TaskCard: React.FC<TaskCardProps> = ({ date, tasks, userAvatar, onTaskTogg
   const [status, setStatus] = useState('');
 
   return (
-    <div className="flex justify-between items-center max-w-xs mx-auto mb-4">
-      {/* User Avatar Section */}
-      <div className="flex-shrink-0 w-20 h-20">
-        {userAvatar ? (
-          <img src={userAvatar} alt="User Avatar" className="w-full h-full rounded-full border-2 border-f5f5f5" />
-        ) : (
-          <img src={avatar} className="w-full h-full bg-gray-300 rounded-full" />
-        )}
+    <div className="flex flex-col bg-white shadow-lg rounded-lg p-6 max-w-lg mx-auto mb-4">
+      {/* Date and Avatar Section */}
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="font-bold text-xl text-black">{date}</h2>
+        <div className="w-10 h-10">
+          {userAvatar ? (
+            <img src={userAvatar} alt="User Avatar" className="w-full h-full rounded-full border-2 border-f5f5f5" />
+          ) : (
+            <img src={avatar} className="w-full h-full bg-gray-300 rounded-full" />
+          )}
+        </div>
       </div>
 
       {/* Tasks Section */}
-      <div className="flex-grow bg-gradient-to-r from-white to-bg-3498db shadow-md rounded-lg p-4">
-        <h2 className="font-bold text-xl text-black mb-3">{date}</h2>
-        <ul>
-          {tasks.length > 0 ? tasks.map((task) => (
-            <li key={task.id} className="flex justify-between items-center mb-2 bg-white rounded p-2 hover:shadow-md transition-shadow">
-              <span className={`text-gray-700`}>
-                {task.task_title} - {task.task_description} - {task.priority} - {task.status}
-              </span>
-              <label className="cursor-pointer">
-                <input
-                  type="checkbox"
-                  className="hidden"
-                  // Replace this logic as necessary to match your data structure and requirements
-                  checked={task.status === 'Complete'}
-                  onChange={() => onTaskToggle && onTaskToggle(task.id)}
-                />
-                <div className={`w-5 h-5 bg-border-f5f5f5 border-2 border-gray-300 rounded ${task.status === 'Complete' ? 'bg-3498db' : ''}`}></div>
-              </label>
-            </li>
-          )) : (
-            <div className="text-center py-4">
-              <p className="mb-4">No tasks for this date.</p>
-            </div>
-          )}
-        </ul>
-        <div className="mt-4 text-center">
-          <input type="text" placeholder="Task Title" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} />
-          <input type="text" placeholder="Task Description" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
-          <input type="text" placeholder="Priority" value={priority} onChange={(e) => setPriority(e.target.value)} />
-          <input type="text" placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)} />
-          <button onClick={() => onAddTask && onAddTask(taskTitle, taskDescription, priority, status)} className="bg-3498db text-white px-4 py-2 rounded shadow hover:bg-opacity-90 transition ease-in-out duration-150 focus:outline-none">
-            Add Task
-          </button>
-        </div>
+      <ul className="mb-4">
+        {tasks.length > 0 ? tasks.map((task) => (
+          <li key={task.id} className="flex justify-between items-center mb-2 p-2 bg-gray-100 rounded hover:bg-gray-200 transition-colors">
+            <span className={`text-gray-700 ${task.status === 'Complete' ? 'line-through' : ''}`}>
+              {task.task_title}
+            </span>
+            <label className="cursor-pointer flex items-center">
+              <input
+                type="checkbox"
+                className="hidden"
+                checked={task.status === 'Complete'}
+                onChange={() => onTaskToggle && onTaskToggle(task.id)}
+              />
+              {task.status === 'Complete' ? <FaCheckCircle className="text-green-500" /> : <FaTimesCircle className="text-red-500" />}
+            </label>
+          </li>
+        )) : (
+          <div className="text-center py-4">
+            <p className="mb-4">No tasks for this date.</p>
+          </div>
+        )}
+      </ul>
+
+      {/* Add Task Section */}
+      <div className="border-t pt-4">
+        <input type="text" placeholder="Task Title" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} className="w-full p-2 mb-2 border rounded" />
+        <input type="text" placeholder="Task Description" value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} className="w-full p-2 mb-2 border rounded" />
+        <input type="text" placeholder="Priority" value={priority} onChange={(e) => setPriority(e.target.value)} className="w-full p-2 mb-2 border rounded" />
+        <input type="text" placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)} className="w-full p-2 mb-2 border rounded" />
+        <button onClick={() => onAddTask && onAddTask(taskTitle, taskDescription, priority, status)} className="w-full bg-3498db text-white p-2 rounded shadow hover:bg-opacity-90 transition ease-in-out duration-150 focus:outline-none">
+          Add Task
+        </button>
       </div>
     </div>
   );
