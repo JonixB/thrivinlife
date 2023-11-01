b68312e49c5764200cb7c9ba457ffd5501bdc292
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-01 12:32:27 -0600

    create confirmation modal

---
+++ src/components/DeleteConfirmation.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+interface DeleteConfirmationProps {
+  show: boolean;
+  onCancel: () => void;
+  onConfirm: () => void;
+}
+
+const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({ show, onCancel, onConfirm }) => {
+  if (!show) return null;
+
+  return (
+    <div className="fixed z-10 inset-0 overflow-y-auto">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity">
+          <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-lg leading-6 font-medium text-gray-900">
+                  Delete Task
+                </h3>
+                <div className="mt-2">
+                  <p className="text-sm leading-5 text-gray-500">
+                    Are you sure you want to delete this task?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+            <span className="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
+              <button onClick={onConfirm} type="button" className="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-red-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                Delete
+              </button>
+            </span>
+            <span className="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
+              <button onClick={onCancel} type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 px-4 py-2 bg-white text-base leading-6 font-medium text-gray-700 shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                Cancel
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteConfirmation;
