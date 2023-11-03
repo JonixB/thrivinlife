2ce76840612b1e3b39a14e14ea66dd380374509e
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-02 19:21:18 -0600

    add SideBar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface Props {
+  avatarUrl: string | null;
+  userName: string | undefined;
+}
+
+const Sidebar: React.FC<Props> = ({ avatarUrl, userName }) => {
+  return (
+    <div className="flex flex-col w-60 bg-gray-900 text-white pr-8">
+      <div className="flex items-center mb-4">
+        <div className="bg-white p-2 rounded-full mr-4">
+          <img src={avatarUrl || 'default-avatar-url'} alt="User Avatar" width="40" height="40" />
+        </div>
+        <div className="text-xl font-semibold text-white">{userName}</div>
+      </div>
+      <ul className="space-y-4">
+        <li>Daily Tasks</li>
+        <li>Budgeting</li>
+        <li>Health</li>
+      </ul>
+      <button className="mt-auto text-gray-400">Sign out</button>
+    </div>
+  );
+};
+
+export default Sidebar;
