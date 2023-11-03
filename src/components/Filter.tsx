f619d05ca746a278a59ef441851a4b83896c00c1
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-02 19:20:37 -0600

    add new filter components

---
+++ src/components/Filter.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FilterSection: React.FC = () => {
+  return (
+    <div className="w-1/3 p-8 bg-white rounded-xl shadow-lg">
+      <h2 className="text-xl font-semibold mb-4">Filter by</h2>
+      {/* ... filter content ... */}
+    </div>
+  );
+};
+
+export default FilterSection;
