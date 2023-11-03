3303af1e00c5c796d65f186c679ca09028eec4c9
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-02 19:23:08 -0600

    remove blur effect

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="fixed bottom-0 left-0 w-full p-4 bg-opacity-50 backdrop-blur-md z-50">
+    <footer className="fixed bottom-0 left-0 w-full p-4 bg-opacity-50 z-50">
       <div className="max-w-xl mx-auto flex justify-center items-center">
         <span className="text-gray-700">
           <span className="text-black">&#169;</span> ThrivinLife 2023
