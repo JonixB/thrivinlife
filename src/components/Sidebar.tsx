297a125277ef5e0d73138b670a931ad54e7f0926
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-04 14:46:11 -0600

    restyle sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaTasks, FaWallet, FaSignOutAlt } from 'react-icons/fa';
+import { MdFitnessCenter } from 'react-icons/md';
 
 interface Props {
   avatarUrl: string | null;
@@ -7,19 +9,27 @@ interface Props {
 
 const Sidebar: React.FC<Props> = ({ avatarUrl, userName }) => {
   return (
-    <div className="flex flex-col w-60 bg-gray-100 text-black pr-8">
+    <div className="flex flex-col w-60 bg-gray-100 text-black pr-8 rounded-lg shadow-lg p-4">
       <div className="flex items-center mb-4">
         <div className="bg-white p-2 rounded-full mr-4">
           <img src={avatarUrl || 'default-avatar-url'} alt="User Avatar" width="40" height="40" />
         </div>
         <div className="text-xl font-semibold text-white">{userName}</div>
       </div>
-      <ul className="space-y-4">
-        <li>Daily Tasks</li>
-        <li>Budgeting</li>
-        <li>Health</li>
+      <ul className="space-y-4 text-gray-700">
+        <li className="flex items-center">
+          <FaTasks className="mr-2" /> Daily Tasks
+        </li>
+        <li className="flex items-center">
+          <FaWallet className="mr-2" /> Budgeting
+        </li>
+        <li className="flex items-center">
+          <MdFitnessCenter className="mr-2" /> Health
+        </li>
       </ul>
-      <button className="mt-auto text-gray-400">Sign out</button>
+      <button className="mt-auto text-gray-400 flex items-center">
+        <FaSignOutAlt className="mr-2" /> Sign out
+      </button>
     </div>
   );
 };
