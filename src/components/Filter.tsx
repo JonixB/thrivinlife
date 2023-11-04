6069972387f774a3b1352f8c07f65fdd8fdf274b
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-04 15:50:57 -0600

    moving the calendar to the filter column

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Calendar from 'react-calendar';
+import { TaskContext } from '../context/TaskContext';
+import { useTaskContext } from '../hooks/useTaskContext';
 
 const FilterSection: React.FC = () => {
+  const { selectedDate, setSelectedDate, fetchTasksForDate } = useTaskContext();
+
+  type ValuePiece = Date | null;
+  type Value = ValuePiece | ValuePiece[];
+
+  const handleDateChange: (value: Value, event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void =
+    (value, event) => {
+      const date = Array.isArray(value) ? value[0] : value;
+      if (date) {
+        setSelectedDate(date);
+        fetchTasksForDate(date);
+      }
+    };
+
   return (
     <div className="w-1/3 p-8 bg-white rounded-xl shadow-lg">
       <h2 className="text-xl font-semibold mb-4">Filter by</h2>
-      {/* ... filter content ... */}
+      <Calendar
+        onChange={handleDateChange}
+        value={selectedDate}
+      />
     </div>
   );
 };
