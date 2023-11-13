d94d0144dabecb39786017b6e718d6821dc602d4
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-13 13:52:28 -0700

    gave delete modal a higher z inder

--- src/components/DeleteConfirmation.tsx
+++ src/components/DeleteConfirmation.tsx
@@ -10,7 +10,7 @@ const DeleteConfirmation: React.FC<DeleteConfirmationProps> = ({ show, onCancel,
   if (!show) return null;
 
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
+    <div className="fixed z-50 inset-0 overflow-y-auto">
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         <div className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
