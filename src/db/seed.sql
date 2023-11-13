c3f83a0cba7eb95e50d55b19e5e848da486352da
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-13 14:43:11 -0700

    add created_at and updated_at column

--- src/db/seed.sql
+++ src/db/seed.sql
@@ -5,7 +5,11 @@ DROP TABLE IF EXISTS profiles, tasks CASCADE;
 CREATE TABLE profiles (
   user_id uuid REFERENCES auth.users(id),
   profile_image TEXT,
-  PRIMARY KEY (user_id)
+  PRIMARY KEY (user_id),
+  first_name TEXT,
+  last_name TEXT,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 CREATE TABLE tasks (
@@ -15,15 +19,20 @@ CREATE TABLE tasks (
   task_description TEXT,
   due_date DATE,
   priority INT2,
-  status VARCHAR(255)
+  status VARCHAR(255),
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT now(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT now()
 );
 
 -- Seed data
-INSERT INTO profiles (user_id, profile_image)
+INSERT INTO profiles (user_id, profile_image, first_name, last_name)
 VALUES
-    ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'https://avatars.githubusercontent.com/u/113629390?s=400&u=4a9ec4e8bca76b6c78f36952c3b973f34b3ff8c6&v=4');
+    ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'https://avatars.githubusercontent.com/u/113629390?s=400&u=4a9ec4e8bca76b6c78f36952c3b973f34b3ff8c6&v=4', 'Nikko', 'Bansil');
 
 INSERT INTO tasks (user_id, task_title, task_description, due_date, priority, status)
 VALUES
     ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'Coding', 'Keep coding to enhance my skills', '2023-10-31', 4, 'Complete'),
-    ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'Work on project', 'Start doing project features little by little', '2023-10-31', 5, 'Complete');
+    ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'Work on project', 'Start doing project features little by little', '2023-10-31', 5, 'Complete'),
+    ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'Fix task form', 'Fix the sidebar as its also highlighted when adding new task', '2023-11-13', 4, 'Complete'),
+    ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'Email', 'Display email instead of "Your Name"', '2023-11-13', 4, 'Complete'),
+    ('6b869d31-4c41-49bf-8be6-4cbb48d42249', 'Task Summary', 'Make task summary to re-render on every update', '2023-11-13', 5, 'Complete');
