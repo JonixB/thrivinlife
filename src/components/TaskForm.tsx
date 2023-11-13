de6b63118297f23e6d980b51b5d0305749759481
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-13 12:43:38 -0700

    gave form modal a high z index

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -49,7 +49,7 @@ const TaskForm: React.FC<TaskFormProps> = ({ show, onClose, onSubmit, initialTas
   if (!show) return null;
 
   return (
-    <div className="fixed z-10 inset-0 overflow-y-auto">
+    <div className="fixed z-50 inset-0 overflow-y-auto">
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
         
         <div className="fixed inset-0 transition-opacity" onClick={onClose}>
