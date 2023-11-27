326ea8270b2a4de77ce3632717583dbd5346f0b2
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-27 16:54:04 -0700

    create the add function and linked the income form

--- src/components/IncomesList.tsx
+++ src/components/IncomesList.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
-//import { IncomeEntry } from './IncomeEntry'; To be created
+import React, { useState } from 'react';
+import IncomeForm from './IncomeForm';
+import { supabase } from '../lib/helper/supabase';
+import { toast } from 'react-toastify';
+import { useTaskContext } from '../hooks/useTaskContext';
+const { userId } = useTaskContext();
 
 const IncomesList: React.FC = () => {
+  const [isIncomeFormOpen, setIncomeFormOpen] = useState(false);
+  
+
+  const handleOpenIncomeForm = () => {
+    setIncomeFormOpen(true);
+  };
+
+  const handleIncomeFormSubmit = async (date: string, amount: number, category: string, notes: string) => {
+    if (!userId) {
+      console.error('User ID is not available');
+      return;
+    }
+    
+    const newIncomeData = {
+      user_id: userId,
+      date,
+      amount,
+      category,
+      notes
+    };
+
+    const { error } = await supabase.from('incomes').insert([newIncomeData]);
+
+    if (error) {
+      console.error('Error adding income:', error);
+      toast.error('Failed to add income.');
+      return;
+    }
+
+    toast.success('Income added successfully.');
+    setIncomeFormOpen(false);
+  };
+
   return (
     <div className="p-4">
       <h2 className="text-lg font-semibold">Income</h2>
-      {/* List of income entries */}
-      <div className="mt-2">
-        {/* This will be replaced with actual data mapping */}
-        {/*<IncomeEntry />*/}
-        {/*<IncomeEntry />*/}
-      </div>
-      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      {/* Your income list rendering */}
+      <button
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        onClick={handleOpenIncomeForm}
+      >
         Add Income
       </button>
+      <IncomeForm
+        show={isIncomeFormOpen}
+        onClose={() => setIncomeFormOpen(false)}
+        onSubmit={handleIncomeFormSubmit}
+      />
     </div>
   );
 };
