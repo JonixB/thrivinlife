b0efe34e1440882fc744533053c5584635575f1d
Nikko Bansil <bjohnnikko@gmail.com>
2023-11-30 16:44:01 -0700

    lifted selected date

--- src/components/BudgetingComps.tsx
+++ src/components/BudgetingComps.tsx
@@ -1,15 +1,23 @@
+import React, { useState } from 'react';
 import IncomesList from './IncomesList';
 import ExpensesList from './ExpensesList';
 import BudgetSummary from './BudgetSummary';
 
 const BudgetingComps = () => {
+  const [selectedMonth, setSelectedMonth] = useState(getCurrentMonth());
+
   return (
     <div>
-      <IncomesList />
-      <ExpensesList />
-      <BudgetSummary />
+      <BudgetSummary selectedMonth={selectedMonth} setSelectedMonth={setSelectedMonth} />
+      <IncomesList selectedMonth={selectedMonth} />
+      <ExpensesList selectedMonth={selectedMonth} />
     </div>
   );
+};
+
+const getCurrentMonth = () => {
+  const date = new Date();
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
 };
 
 export default BudgetingComps;
