4bcb67b5ebb685ea268f3d1658bb3de3f9291555
Nikko Bansil <bjohnnikko@gmail.com>
2023-12-12 11:01:26 -0700

    setup profile navigation and states

--- src/components/ProfileSetup.tsx
+++ src/components/ProfileSetup.tsx
@@ -1,20 +1,21 @@
 import React, { useState } from 'react';
 import { supabase } from '../lib/helper/supabase';
 import { toast } from 'react-toastify';
-import { Session } from '@supabase/supabase-js';
-
-interface ProfileInfo {
-  
-}
+import { Session, User } from '@supabase/supabase-js';
+import { useNavigate } from 'react-router-dom';
 
 interface ProfileSetupProps {
-  user: Session['user'];
+  user: User | null; // Allow for a null user
 }
 
 const ProfileSetup: React.FC<ProfileSetupProps> = ({ user }) => {
-  const [profileInfo, setProfileInfo] = useState<ProfileInfo>({ /* initial state */ });
+  const [profileInfo, setProfileInfo] = useState<User | null>(null);
+
+  const navigate = useNavigate();
 
   const handleProfileSubmit = async () => {
+    if (!user) return;
+
     const { data, error } = await supabase
       .from('profiles')
       .upsert({ user_id: user.id, ...profileInfo });
@@ -24,7 +25,7 @@ const ProfileSetup: React.FC<ProfileSetupProps> = ({ user }) => {
       console.error(error);
     } else {
       toast.success('Profile updated successfully');
-      // Redirect to main content or dashboard
+      navigate('/');
     }
   };
 
@@ -32,6 +33,7 @@ const ProfileSetup: React.FC<ProfileSetupProps> = ({ user }) => {
   return (
     <div>
       {/* Form elements and submit button */}
+      <button onClick={handleProfileSubmit}>Submit Profile</button>
     </div>
   );
 };
