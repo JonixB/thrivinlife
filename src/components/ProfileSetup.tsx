4546c7b5ac6a659190084e6ea49f241abcc09a91
Nikko Bansil <bjohnnikko@gmail.com>
2023-12-12 11:56:40 -0700

    create the form for the profile's page

--- src/components/ProfileSetup.tsx
+++ src/components/ProfileSetup.tsx
@@ -5,18 +5,37 @@ import { Session, User } from '@supabase/supabase-js';
 import { useNavigate } from 'react-router-dom';
 
 interface ProfileSetupProps {
-  user: User | null; // Allow for a null user
+  user: User | null;
+}
+
+interface ProfileInfo {
+  first_name: string;
+  last_name: string;
+  profile_image: string;
+  date_of_birth: string;
 }
 
 const ProfileSetup: React.FC<ProfileSetupProps> = ({ user }) => {
-  const [profileInfo, setProfileInfo] = useState<User | null>(null);
+  const [profileInfo, setProfileInfo] = useState<ProfileInfo>({
+    first_name: '',
+    last_name: '',
+    profile_image: '',
+    date_of_birth: '',
+  });
 
   const navigate = useNavigate();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProfileInfo({
+      ...profileInfo,
+      [e.target.name]: e.target.value
+    });
+  };
 
   const handleProfileSubmit = async () => {
     if (!user) return;
 
-    const { data, error } = await supabase
+    const { error } = await supabase
       .from('profiles')
       .upsert({ user_id: user.id, ...profileInfo });
 
@@ -29,11 +48,41 @@ const ProfileSetup: React.FC<ProfileSetupProps> = ({ user }) => {
     }
   };
 
-  // Form JSX here
   return (
     <div>
-      {/* Form elements and submit button */}
-      <button onClick={handleProfileSubmit}>Submit Profile</button>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleProfileSubmit();
+      }}>
+        <input
+          type="text"
+          name="first_name"
+          placeholder="First Name"
+          value={profileInfo.first_name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="last_name"
+          placeholder="Last Name"
+          value={profileInfo.last_name}
+          onChange={handleInputChange}
+        />
+        <input
+          type="text"
+          name="profile_image"
+          placeholder="Profile Image URL"
+          value={profileInfo.profile_image}
+          onChange={handleInputChange}
+        />
+        <input
+          type="date"
+          name="date_of_birth"
+          value={profileInfo.date_of_birth}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Submit Profile</button>
+      </form>
     </div>
   );
 };
