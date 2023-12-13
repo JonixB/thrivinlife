b20edf10fa0560d7cacc243f9dc94cee5703f268
Nikko Bansil <bjohnnikko@gmail.com>
2023-12-13 13:48:54 -0700

    change Submit to Save

--- src/components/ProfileSetup.tsx
+++ src/components/ProfileSetup.tsx
@@ -89,7 +89,7 @@ const ProfileSetup: React.FC<ProfileSetupProps> = ({ user }) => {
           />
           <div className="flex justify-end mt-6">
             <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-              Submit Profile
+              Save Profile
             </button>
           </div>
         </form>
