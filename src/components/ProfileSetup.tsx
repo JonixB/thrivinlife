e1cbed5ceefda5e8e67c6a8612a0e48d8eb9486f
Nikko Bansil <bjohnnikko@gmail.com>
2023-12-13 13:48:34 -0700

    restyle the profile form

--- src/components/ProfileSetup.tsx
+++ src/components/ProfileSetup.tsx
@@ -49,40 +49,51 @@ const ProfileSetup: React.FC<ProfileSetupProps> = ({ user }) => {
   };
 
   return (
-    <div>
-      <form onSubmit={(e) => {
-        e.preventDefault();
-        handleProfileSubmit();
-      }}>
-        <input
-          type="text"
-          name="first_name"
-          placeholder="First Name"
-          value={profileInfo.first_name}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="last_name"
-          placeholder="Last Name"
-          value={profileInfo.last_name}
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="profile_image"
-          placeholder="Profile Image URL"
-          value={profileInfo.profile_image}
-          onChange={handleInputChange}
-        />
-        <input
-          type="date"
-          name="date_of_birth"
-          value={profileInfo.date_of_birth}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Submit Profile</button>
-      </form>
+    <div className="fixed inset-0 flex items-center justify-center p-4">
+      <div className="bg-white rounded-lg shadow-xl p-6 w-full max-w-md">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">Profile Setup</h2>
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          handleProfileSubmit();
+        }} className="space-y-4">
+          <input
+            type="text"
+            name="first_name"
+            placeholder="First Name"
+            value={profileInfo.first_name}
+            onChange={handleInputChange}
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="text"
+            name="last_name"
+            placeholder="Last Name"
+            value={profileInfo.last_name}
+            onChange={handleInputChange}
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="text"
+            name="profile_image"
+            placeholder="Profile Image URL"
+            value={profileInfo.profile_image}
+            onChange={handleInputChange}
+            className="w-full p-2 border rounded"
+          />
+          <input
+            type="date"
+            name="date_of_birth"
+            value={profileInfo.date_of_birth}
+            onChange={handleInputChange}
+            className="w-full p-2 border rounded"
+          />
+          <div className="flex justify-end mt-6">
+            <button type="submit" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+              Submit Profile
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
