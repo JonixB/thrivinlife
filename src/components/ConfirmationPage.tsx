30873f9ddf2035517322a480e39704e8bd6cb3b6
Nikko Bansil <bjohnnikko@gmail.com>
2023-12-21 15:43:28 -0700

    create confirmation page component

---
+++ src/components/ConfirmationPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ConfirmationPage: React.FC = () => {
+
+  return (
+    <div className="confirmation-container">
+      <h1>Please Check Your Email</h1>
+      <p>We've sent a confirmation email to the address you provided. Please click the link in that email to proceed with setting up your profile.</p>
+    </div>
+  );
+};
+
+export default ConfirmationPage;
