efde172aa09ae33f0d890922a938dc4c699265d9
Nikko Bansil <bjohnnikko@gmail.com>
2024-01-02 14:49:29 -0700

    add option to select year

--- src/components/BudgetingComps.tsx
+++ src/components/BudgetingComps.tsx
@@ -5,10 +5,16 @@ import BudgetSummary from './BudgetSummary';
 
 const BudgetingComps = () => {
   const [selectedMonth, setSelectedMonth] = useState(getCurrentMonth());
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear().toString());
 
   return (
     <div>
-      <BudgetSummary selectedMonth={selectedMonth} setSelectedMonth={setSelectedMonth} />
+      <BudgetSummary
+        selectedMonth={selectedMonth}
+        setSelectedMonth={setSelectedMonth}
+        selectedYear={selectedYear}
+        setSelectedYear={setSelectedYear}
+      />
       <IncomesList selectedMonth={selectedMonth} />
       <ExpensesList selectedMonth={selectedMonth} />
     </div>
@@ -17,7 +23,16 @@ const BudgetingComps = () => {
 
 const getCurrentMonth = () => {
   const date = new Date();
-  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}`;
+  return String(date.getMonth() + 1).padStart(2, '0');
+};
+
+const getYearOptions = () => {
+  const currentYear = new Date().getFullYear();
+  const years = [];
+  for (let i = currentYear - 10; i <= currentYear + 10; i++) {
+    years.push(i.toString());
+  }
+  return years;
 };
 
 export default BudgetingComps;
